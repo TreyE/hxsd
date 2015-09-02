8290f70792834c8ee0180c24d13491c84417d62f
Trey Evans <lewis.r.evans@gmail.com>
2015-09-02 12:54:50 -0400

    Updating types.

--- src/Hxsd/Types.hs
+++ src/Hxsd/Types.hs
@@ -3,5 +3,12 @@ module Hxsd.Types where
 data XmlParsingFailure = XmlParsingFailure deriving Eq
 data SchemaLoadFailure = SchemaLoadFailure deriving Eq
 
+data XmlSchemaValidationResult = XmlIsSchemaValid
+                                 | XmlFailsSchemaValidation [String]
+
+data XmlAgainstSchemaFileResult = XmlSchemaValidationCompleted XmlSchemaValidationResult
+                                  | XmlDataParseFailure XmlParsingFailure
+                                  | SchemaFileLoadFailure SchemaLoadFailure
+
 type XmlParsingResult a = Either XmlParsingFailure a
 type SchemaLoadResult a = Either SchemaLoadFailure a
